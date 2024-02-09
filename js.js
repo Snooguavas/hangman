d83923165126713b85605aae34bc6380a958063f
Snooguavas <michael.avidan39@gmail.com>
2024-02-09 11:50:24 +0200

    First commit

---
+++ js.js
@@ -0,0 +1,63 @@
+const words = ['mefagauker', 'orangie', 'machalot', 'tambal', 'fucker'];
+// const letters =['a','b','c','d','e','f','g','h','i',
+// 'j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+function chooseWord () {
+    const rand = Math.floor(Math.random() * words.length);
+    return words[rand];
+}
+let attemptsLeft;
+let answerText = document.querySelector('#answer');
+
+const playAgainBtn = document.querySelector('#playagain');
+playAgainBtn.addEventListener('click', playGame);
+
+function playGame() {
+    // initialize game
+    let solution = chooseWord();
+    attemptsLeft = 5;
+    console.log(solution);
+    let answer = [];
+    for (let char of solution){
+        answer.push('_ ');
+    }
+    function updateStats() {
+        document.querySelector('.lives').textContent = `Lives: ${attemptsLeft}`;
+        answerText.textContent = answer.join(' ');
+    }
+    updateStats();
+    // listen to buttons
+    const buttons = document.querySelectorAll('#letters_container .btn');
+    for (let button of buttons) {
+        button.addEventListener('click', checkLetter);
+    }
+    
+    
+    // checking attempted letter 
+    function checkLetter() {
+        const letterTried = this.textContent.toLowerCase();
+        if (!solution.includes(letterTried)){
+            console.log('Nope!');
+            attemptsLeft--;
+        }
+        // if right
+        else {
+            for (let i = 0; i < solution.length; i++) {
+                if (letterTried === solution[i]) {
+                    answer[i] = solution[i];
+                    console.log(`${solution[i]} was found!`)
+                }
+            }
+            answerText.textContent = answer.join(' ');
+        }
+        updateStats();
+        this.disabled = true;
+        if (attemptsLeft === 0) {
+            console.log('You Lose!')
+        }
+        else if (answer.join('') === solution) {
+            console.log('You Win!');
+            words.splice(words.indexOf(solution), 1);
+        }
+    }
+}
+// playGame();
